c8783781d5616bba5844d27cf236fb4587bca190
Megan Yin <my474@cornell.edu>
2020-04-04 18:59:33 -0400

    Finished first landing part (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Result from './Result';
-import './App.css';
-import { ResultType } from './types';
-import { getEmails, addEmail } from './firebase/api';
+import React from 'react';
+import Landing from './Landing';
+import EmailBox from './EmailBox';
+import styles from './App.module.css';
 
-export default (): React.ReactElement => {
-  const [email, setEmail] = useState<string>('');
-  const [emails, setEmails] = useState<string[]>([]);
-  const [result, setResult] = useState<ResultType>('None');
-
-  const fetchData = async (): Promise<void | string[]> => getEmails().then((res) => res);
-
-  useEffect(() => {
-    fetchData()
-      .then((list) => list && setEmails(list));
-  }, []);
-
-  const validEmail = (input: string): boolean => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
-
-  const submitEmail = async (): Promise<void> => {
-    try {
-      setResult('Loading');
-      if (!validEmail(email)) {
-        setResult('invalidInput');
-        return;
-      }
-      if (emails.includes(email)) {
-        setResult('exists');
-        return;
-      }
-      addEmail(email);
-      setEmail('');
-      setResult('Success');
-    } catch (err) {
-      setResult('Error');
-    }
-  };
-
-  return (
-    <div className="App">
-      <input
-        type="text"
-        placeholder="Enter your email"
-        value={email}
-        onChange={(e) => setEmail(e.currentTarget.value)}
-      />
-      <button
-        type="submit"
-        onClick={submitEmail}
-      >
-        Subscribe!
-      </button>
-      <Result result={result} />
-    </div>
-  );
-};
+export default (): React.ReactElement => (
+  <div className={styles.App}>
+    <Landing />
+    <EmailBox />
+  </div>
+);
