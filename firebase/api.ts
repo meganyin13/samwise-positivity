89f48fb40c151d617495a83f77872eefe59abb91
Megan Yin <my474@cornell.edu>
2020-04-03 20:13:27 -0400

    clean up

--- firebase/api.ts
+++ firebase/api.ts
@@ -1,5 +1,6 @@
-const admin = require('firebase-admin');
-const express = require('express');
+import admin from 'firebase-admin';
+import express from 'express';
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 // eslint-disable-next-line import/no-unresolved
@@ -17,13 +18,13 @@ app.use(cors());
 const db = admin.firestore();
 const emailsCollection = db.collection('emails');
 
-app.post('/addEmail', async (req, res) => {
+app.post('/addEmail', async (req: express.Request, res: express.Response) => {
   const { body } = req;
   const addedEmail = await emailsCollection.add(body);
   res.send(addedEmail.id);
 });
 
-app.get('/getEmails', async (req, res) => {
+app.get('/getEmails', async (_: express.Request, res: express.Response) => {
   const emailsDocs = await emailsCollection.get();
   const emails = emailsDocs.docs.map((doc) => doc.data);
   res.send(emails);
