108ab4103177d36bf212ecdc173951472f822da7
Megan Yin <my474@cornell.edu>
2020-04-04 21:48:57 -0400

    add confetti

--- src/EmailBox.tsx
+++ src/EmailBox.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Confetti from 'react-dom-confetti';
 import Result from './Result';
 import styles from './EmailBox.module.css';
 import { ResultType } from './types';
@@ -17,6 +18,22 @@ export default (): React.ReactElement => {
   }, []);
 
   const validEmail = (input: string): boolean => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input);
+
+  const confettiConfig = {
+    angle: 0,
+    spread: 360,
+    startVelocity: 18,
+    elementCount: 200,
+    dragFriction: 0.1,
+    duration: 3000,
+    delay: 0,
+    colors: ['#a864fd', '#29cdff', '#78ff44', '#ff718d', '#fdff6a'],
+  };
+
+  const onInputChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    setResult('None');
+    setEmail(e.currentTarget.value);
+  };
 
   const submitEmail = async (): Promise<void> => {
     try {
@@ -43,7 +60,7 @@ export default (): React.ReactElement => {
         type="text"
         placeholder="enter email"
         value={email}
-        onChange={(e) => setEmail(e.currentTarget.value)}
+        onChange={onInputChange}
         className={styles.Input}
       />
       <button
@@ -53,6 +70,14 @@ export default (): React.ReactElement => {
       >
         Sign Up!
       </button>
+      <div style={{
+        position: 'absolute',
+        left: '50%',
+        top: '10%',
+      }}
+      >
+        <Confetti active={result === 'Success'} config={confettiConfig} />
+      </div>
       <div className={styles.Result}>
         <Result result={result} />
       </div>
