76860d02aa14c594baa6b2b7bc8637472ecb8761
Megan Yin <my474@cornell.edu>
2020-04-12 10:47:28 -0400

    add bear to footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import { ReactComponent as BEARRRR } from './imgs/BEARRRR.svg';
 import logoHeart from './imgs/heart_logo.svg';
 import logoLink from './imgs/link_logo.svg';
 import styles from './Footer.module.css';
 
 export default (): React.ReactElement => (
   <footer className={styles.Footer}>
-    <p>
-      Made by DTI
-      {' '}
-      <img src={logoHeart} alt="with love" />
-    </p>
-    <p>
-      <a href="https://cornelldti.org">
-        Cornell Design &amp; Tech Initiative
+    <BEARRRR className={styles.BEARRRR} />
+    <div>
+      <p>
+        Made by DTI
         {' '}
-        <img src={logoLink} alt="external link" />
-      </a>
-    </p>
+        <img src={logoHeart} alt="with love" />
+      </p>
+      <p>
+        <a href="https://cornelldti.org">
+          Cornell Design &amp; Tech Initiative
+          {' '}
+          <img src={logoLink} alt="external link" />
+        </a>
+      </p>
+    </div>
   </footer>
 );
