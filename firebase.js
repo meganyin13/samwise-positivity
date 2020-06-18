a6f586c0b70cead36dbbd0876f2247f7ed81f98a
Danesh <daneshpbc@gmail.com>
2020-06-18 17:16:10 +0800

    adding change from updateChanges branch

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin')
-const serviceAccount = require('./ServiceAccountKey.json')
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://samwise-positivity.firebaseio.com'
-})
-
-let db = admin.firestore()
