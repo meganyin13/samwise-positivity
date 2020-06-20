ba464ad9fbf88456402b7420a19a119b51069f61
Sunisth Kumar <ksunisth@gmail.com>
2020-06-20 20:33:09 +0530

    added blog

--- src/Components/Landing.tsx
+++ src/Components/Landing.tsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import styles from '../css/Landing.module.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../css/Landing.module.css';
 
-export const Landing = (): React.ReactElement => {
-  return (
-    <div className={styles.Landing}>
-      <NavBar />
-      <div>
-        <h1 className={styles.Title}>
-          #SpreadThe
-          <span className={styles.Love}>Love</span>
-        </h1>
-        <p className={styles.SubText}>
+export const Landing = (): React.ReactElement => (
+  <div className={styles.Landing}>
+    <NavBar />
+    <div>
+      <h1 className={styles.Title}>
+        #SpreadThe
+        <span className={styles.Love}>Love</span>
+      </h1>
+      <p className={styles.SubText}>
         Week of June 20, 2020
-        </p>
-        <p className={styles.SubText}>
-          Happy mid-June. Whether you're stuck at home or your town is reopening, here are some
-          positive headlines and fun activities guaranteed to get you through your day!
-        </p>
-      </div>
+      </p>
+      <p className={styles.SubText}>
+        Happy mid-June. Whether you're stuck at home or your town is reopening, here are some
+        positive headlines and fun activities guaranteed to get you through your day!
+      </p>
     </div>
-  )
-}
+  </div>
+);
 
-export const NavBar = () => {
-  return (
-    <div className={styles.LogoBar}>
-        <h1 className={styles.NavBarTitle}><a href='/'>#SpreadThe
-          <span className={styles.Love}>Love</span></a>
-          <Link to='/subscribe'>
-            <span className={styles.NavBarSubscribe}><a href='/'>Subscribe</a></span>
-          </Link>
-          <Link to='/'>
-            <span className={styles.NavBarItems}><a href='/'>Home</a></span>
-          </Link>
-        </h1>
-     </div>
-  )
-}
+export const NavBar = () => (
+  <div className={styles.LogoBar}>
+    <h1 className={styles.NavBarTitle}>
+      <a href="/">
+        #SpreadThe
+        <span className={styles.Love}>Love</span>
+      </a>
+      <Link to="/subscribe">
+        <span className={styles.NavBarSubscribe}><a href="/">Subscribe</a></span>
+      </Link>
+      <Link to="/Blog">
+        <span className={styles.NavBarItems}><a href="/">Blog</a></span>
+      </Link>
+      <Link to="/">
+        <span className={styles.NavBarItems}><a href="/">Home</a></span>
+      </Link>
+    </h1>
+  </div>
+);
--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import styles from '../css/SignUp.module.css'
-import Newsletter from '../imgs/newsletter.png'
+import React from 'react';
+import styles from '../css/SignUp.module.css';
+import Newsletter from '../imgs/newsletter.png';
 
 export default (): React.ReactElement => (
   <div className={styles.SignUp}>
@@ -8,10 +8,10 @@ export default (): React.ReactElement => (
       <h3 className={styles.PromptTitle}>This week's positivity prompt:</h3>
       <p className={styles.PromptText}>Tell us something inspiring you saw someone else do this week!</p>
       <form className={styles.PromptBox}>
-        <input className={styles.PromptBoxInside} type='textbox' placeholder='Enlighten us by typing here...'></input>
+        <input className={styles.PromptBoxInside} type="textbox" placeholder="Enlighten us by typing here..." />
       </form>
-      <button type='submit' className={styles.Button}>Submit</button>
-      <img className={styles.Image} src={Newsletter} alt='current newsletter'></img>
-      </div>
+      <button type="submit" className={styles.Button}>Submit</button>
+      <img className={styles.Image} src={Newsletter} alt="current newsletter" />
+    </div>
   </div>
-)
+);
