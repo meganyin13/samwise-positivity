a1d5ad5575fb9303a16e26f2af170c28327e783f
Danesh <daneshpbc@gmail.com>
2020-06-24 15:52:39 +0800

    add message function

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styles from '../css/SignUp.module.css';
 import Newsletter from '../imgs/newsletter.png';
+import { addPositiveMessage } from '../firebase/api';
 
 function SignUp(): React.ReactElement {
   const [message, setMessage] = useState('');
@@ -9,16 +10,23 @@ function SignUp(): React.ReactElement {
     event.preventDefault();
     if (message !== '') {
       addPositiveMessage(message);
+      setMessage('');
     }
-  }
+  };
+
+  const onInputChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    setMessage(event.currentTarget.value);
+  };
 
   return (
     <div className={styles.SignUp}>
       <div className={styles.Left}>
-        <h3 className={styles.PromptTitle}>This week's positivity prompt:</h3>
-        <p className={styles.PromptText}>Tell us something inspiring you saw someone else do this week!</p>
-        <form className={styles.PromptBox} onSubmit={this.onSubmit}>
-          <input className={styles.PromptBoxInside} type="textbox" onChange={this.onInputChange} value={this.state.message} placeholder="Enlighten us by typing here..." />
+        <h3 className={styles.PromptTitle}>This week&apos;s positivity prompt:</h3>
+        <p className={styles.PromptText}>
+          Tell us something inspiring you saw someone else do this week!
+        </p>
+        <form className={styles.PromptBox} onSubmit={onSubmit}>
+          <input className={styles.PromptBoxInside} type="textbox" onChange={onInputChange} value={message} placeholder="Enlighten us by typing here..." />
           <button type="submit" className={styles.Button}>Submit</button>
         </form>
         <img className={styles.Image} src={Newsletter} alt="current newsletter" />
