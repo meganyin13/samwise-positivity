c0de019063573c2495147ad643510062d4606bf1
Danesh <daneshpbc@gmail.com>
2020-06-24 15:35:10 +0800

    temp changes

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -2,22 +2,29 @@ import React, { useState } from 'react';
 import styles from '../css/SignUp.module.css';
 import Newsletter from '../imgs/newsletter.png';
 
-const onInputChange = (): void => {
+function SignUp(): React.ReactElement {
+  const [message, setMessage] = useState('');
 
-};
-export default (): React.ReactElement => (
-  <div className={styles.SignUp}>
-    <div className={styles.Left}>
-      <h3 className={styles.PromptTitle}>This week&apos;s positivity prompt:</h3>
-      <p className={styles.PromptText}>
-        Tell us something inspiring
-        you saw someone else do this week!
-      </p>
-      <form className={styles.PromptBox}>
-        <input className={styles.PromptBoxInside} type="textbox" placeholder="Enlighten us by typing here..." />
-      </form>
-      <button type="submit" className={styles.Button}>Submit</button>
-      <img className={styles.Image} src={Newsletter} alt="current newsletter" />
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    if (message !== '') {
+      addPositiveMessage(message);
+    }
+  }
+
+  return (
+    <div className={styles.SignUp}>
+      <div className={styles.Left}>
+        <h3 className={styles.PromptTitle}>This week's positivity prompt:</h3>
+        <p className={styles.PromptText}>Tell us something inspiring you saw someone else do this week!</p>
+        <form className={styles.PromptBox} onSubmit={this.onSubmit}>
+          <input className={styles.PromptBoxInside} type="textbox" onChange={this.onInputChange} value={this.state.message} placeholder="Enlighten us by typing here..." />
+          <button type="submit" className={styles.Button}>Submit</button>
+        </form>
+        <img className={styles.Image} src={Newsletter} alt="current newsletter" />
+      </div>
     </div>
-  </div>
-);
+  );
+}
+
+export default SignUp;
